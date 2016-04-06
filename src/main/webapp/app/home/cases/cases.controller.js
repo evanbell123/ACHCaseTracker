18be9b3720041af05de65080021032a5554a3a3f
Evan Bell <evanbell91@gmail.com>
2016-04-05 19:12:55 -0500

    Update status from grid is working

--- src/main/webapp/app/home/cases/cases.controller.js
+++ src/main/webapp/app/home/cases/cases.controller.js
@@ -167,7 +167,7 @@
                 //data.status = getCaseStatusLabel(status);
                 //data.type = getCaseTypeLabel(type);
 
-                console.log(rowEntity);
+                console.log(rowEntity.status, newValue);
 
                 $http({
                     method: 'PUT',
@@ -175,6 +175,7 @@
                     data: rowEntity
                 }).then(function successCallback(response) {
 
+                    console.log(response.data.status);
 
                     $scope.msg.lastCellEdited = 'You changed ' + colDef.displayName + ' of case number ' + rowEntity.id + ' from ' + oldValue + ' to ' + newValue;
                 }, function errorCallback(response) {
@@ -206,16 +207,22 @@
 
                 for (var i = 0; i < data.length; i++) {
                     //console.log(data[i].status);
-                    data[i].status = getCaseStatusValue(data[i].status);
-                    data[i].type = getCaseTypeValue(data[i].type);
+                    data[i].status = getEnumIdFromName(Enums.CaseStatus, data[i].status);
+                    data[i].type = getEnumIdFromName(Enums.CaseType, data[i].type);
                     //console.log(data[i].type);
                     data[i].lastPaymentOn = new Date(data[i].lastPaymentOn);
                     data[i].slaDeadline = new Date(data[i].slaDeadline);
                 }
                 $scope.gridOptions.data = data;
             });
+    }
 
+    function getEnumIdFromName(CaseEnum, name) {
+        var enumId = CaseEnum.filter(function(value) {
+            return value.name === name;
+        })[0].id;
 
+        return enumId;
     }
 
     function dropdownEditorOptions(CaseEnum) {
@@ -244,132 +251,5 @@
         }
         console.log(selectOptions);
         return selectOptions;
-    }
-
-    function getCaseStatusValue(statusLabel) {
-        switch (statusLabel) {
-            case 'OPEN':
-                return 1;
-                break;
-            case 'IN_PROGRESS':
-                return 2;
-                break;
-            case 'CLOSED':
-                return 3;
-                break;
-            default:
-                return statusLabel;
-                break;
-
-        }
-    }
-
-    function getCaseStatusLabel(statusValue) {
-        switch (statusValue) {
-            case 1:
-                return 'OPEN';
-                break;
-            case 2:
-                return 'IN_PROGRESS';
-                break;
-            case 3:
-                return 'CLOSED';
-                break;
-            default:
-                return statusValue;
-                break;
-        }
-    }
-
-    function getCaseTypeValue(typeLabel) {
-        switch (typeLabel) {
-            case 'GOV_REC':
-                return 1;
-                break;
-            case 'POA':
-                return 2;
-                break;
-            case 'REV_DEL':
-                return 3;
-                break;
-            case 'RETURN':
-                return 4;
-                break;
-            case 'UNRESOLVED':
-                return 5;
-                break;
-            default:
-                return typeLabel;
-                break;
-        }
-    }
-
-    function getCaseTypeLabel(typeValue) {
-        switch (typeValue) {
-            case 1:
-                return 'GOV_REC';
-                break;
-            case 2:
-                return 'POA';
-                break;
-            case 3:
-                return 'REV_DEL';
-                break;
-            case 4:
-                return 'RETURN';
-                break;
-            case 5:
-                return 'UNRESOLVED';
-                break;
-            default:
-                return typeValue;
-                break;
-        }
-    }
-
-    function getCaseSubtypeValue(subtypeLabel) {
-        switch (subtypeLabel) {
-            case 'GOV_REC':
-                return 1;
-                break;
-            case 'DNE':
-                return 2;
-                break;
-            case 'CRF':
-                return 3;
-                break;
-            case 'TREAS_REFERRAL':
-                return 4;
-                break;
-            case 'TREAS_REFUND':
-                return 5;
-                break;
-            default:
-                return subtypeLabel;
-                break;
-        }
-    }
-
-    function getCaseSubtypeLabel(subtypeValue) {
-        switch (subtypeValue) {
-            case 1:
-                return 'GOV_REC';
-                break;
-            case 2:
-                return 'DNE';
-                break;
-            case 3:
-                return 'CRF';
-                break;
-            case 4:
-                return 'TREAS_REFERRAL';
-                break;
-            case 5:
-                return 'TREAS_REFUND';
-                break;
-            default:
-                return subtypeValue;
-                break;
-        }
     }
 })();
