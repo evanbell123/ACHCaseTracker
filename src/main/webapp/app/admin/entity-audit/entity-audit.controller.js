54de914cb8c4eeb7cb3b54476785d41837f1c269
Kienc123 <ragnarok_50@hotmail.com>
2016-05-02 22:10:56 -0500

    Add dynamic table creation using Audit Entity

--- src/main/webapp/app/admin/entity-audit/entity-audit.controller.js
+++ src/main/webapp/app/admin/entity-audit/entity-audit.controller.js
@@ -113,236 +113,71 @@
         }
 
         function exportPDF() {
-            var docDefinition =  {
-                content: [
-                    { text: vm.getEntityName(vm.qualifiedName), style: 'header' },
-                    'Official documentation is in progress, this document is just a glimpse of what is possible with pdfmake and its layout engine.',
-                    { text: 'A simple table (no headers, no width specified, no spans, no styling)', style: 'subheader' },
-                    'The following table has nothing more than a body array',
-                    {
-                        style: 'tableExample',
-                        table: {
-                            body: [
-                                ['Column 1', 'Column 2', 'Column 3'],
-                                ['One value goes here', 'Another one here', 'OK?']
-                            ]
-                        }
-                    },
-                    { text: 'A simple table with nested elements', style: 'subheader' },
-                    'It is of course possible to nest any other type of nodes available in pdfmake inside table cells',
-                    {
-                        style: 'tableExample',
-                        table: {
-                            body: [
-                                ['Column 1', 'Column 2', 'Column 3'],
-                                [
-                                    {
-                                        stack: [
-                                            'Let\'s try an unordered list',
-                                            {
-                                                ul: [
-                                                    'item 1',
-                                                    'item 2'
-                                                ]
-                                            }
-                                        ]
-                                    },
-                                    [
-                                        'or a nested table',
-                                        {
-                                            table: {
-                                                body: [
-                                                    [ 'Col1', 'Col2', 'Col3'],
-                                                    [ '1', '2', '3'],
-                                                    [ '1', '2', '3']
-                                                ]
-                                            },
-                                        }
-                                    ],
-                                    { text: [
-                                        'Inlines can be ',
-                                        { text: 'styled\n', italics: true },
-                                        { text: 'easily as everywhere else', fontSize: 10 } ]
-                                    }
-                                ]
-                            ]
-                        }
-                    },
-                    { text: 'Defining column widths', style: 'subheader' },
-                    'Tables support the same width definitions as standard columns:',
-                    {
-                        bold: true,
-                        ul: [
-                            'auto',
-                            'star',
-                            'fixed value'
-                        ]
-                    },
-                    {
-                        style: 'tableExample',
-                        table: {
-                            widths: [100, '*', 200, '*'],
-                            body: [
-                                [ 'width=100', 'star-sized', 'width=200', 'star-sized'],
-                                [ 'fixed-width cells have exactly the specified width', { text: 'nothing interesting here', italics: true, color: 'gray' }, { text: 'nothing interesting here', italics: true, color: 'gray' }, { text: 'nothing interesting here', italics: true, color: 'gray' }]
-                            ]
-                        }
-                    },
-                    { text: 'Headers', style: 'subheader' },
-                    'You can declare how many rows should be treated as a header. Headers are automatically repeated on the following pages',
-                    { text: [ 'It is also possible to set keepWithHeaderRows to make sure there will be no page-break between the header and these rows. Take a look at the document-definition and play with it. If you set it to one, the following table will automatically start on the next page, since there\'s not enough space for the first row to be rendered here' ], color: 'gray', italics: true },
-                    {
-                        style: 'tableExample',
-                        table: {
-                            headerRows: 1,
-                            // keepWithHeaderRows: 1,
-                            // dontBreakRows: true,
-                            body: [
-                                [{ text: 'Header 1', style: 'tableHeader' }, { text: 'Header 2', style: 'tableHeader' }, { text: 'Header 3', style: 'tableHeader' }],
-                                [
-                                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-                                ]
-                            ]
-                        }
-                    },
-                    { text: 'Column/row spans', style: 'subheader' },
-                    'Each cell-element can set a rowSpan or colSpan',
-                    {
-                        style: 'tableExample',
-                        color: '#444',
-                        table: {
-                            widths: [ 200, 'auto', 'auto' ],
-                            headerRows: 2,
-                            // keepWithHeaderRows: 1,
-                            body: [
-                                [{ text: 'Header with Colspan = 2', style: 'tableHeader', colSpan: 2, alignment: 'center' }, {}, { text: 'Header 3', style: 'tableHeader', alignment: 'center' }],
-                                [{ text: 'Header 1', style: 'tableHeader', alignment: 'center' }, { text: 'Header 2', style: 'tableHeader', alignment: 'center' }, { text: 'Header 3', style: 'tableHeader', alignment: 'center' }],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ { rowSpan: 3, text: 'rowSpan set to 3\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor' }, 'Sample value 2', 'Sample value 3' ],
-                                [ '', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', { colSpan: 2, rowSpan: 2, text: 'Both:\nrowSpan and colSpan\ncan be defined at the same time' }, '' ],
-                                [ 'Sample value 1', '', '' ],
-                            ]
-                        }
-                    },
-                    { text: 'Styling tables', style: 'subheader' },
-                    'You can provide a custom styler for the table. Currently it supports:',
-                    {
-                        ul: [
-                            'line widths',
-                            'line colors',
-                            'cell paddings',
-                        ]
-                    },
-                    'with more options coming soon...\n\npdfmake currently has a few predefined styles (see them on the next page)',
-                    { text: 'noBorders:', fontSize: 14, bold: true, pageBreak: 'before', margin: [0, 0, 0, 8] },
-                    {
-                        style: 'tableExample',
-                        table: {
-                            headerRows: 1,
-                            body: [
-                                [{ text: 'Header 1', style: 'tableHeader' }, { text: 'Header 2', style: 'tableHeader'}, { text: 'Header 3', style: 'tableHeader' }],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                            ]
-                        },
-                        layout: 'noBorders'
-                    },
-                    { text: 'headerLineOnly:', fontSize: 14, bold: true, margin: [0, 20, 0, 8] },
-                    {
-                        style: 'tableExample',
-                        table: {
-                            headerRows: 1,
-                            body: [
-                                [{ text: 'Header 1', style: 'tableHeader' }, { text: 'Header 2', style: 'tableHeader'}, { text: 'Header 3', style: 'tableHeader' }],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                            ]
-                        },
-                        layout: 'headerLineOnly'
-                    },
-                    { text: 'lightHorizontalLines:', fontSize: 14, bold: true, margin: [0, 20, 0, 8] },
-                    {
-                        style: 'tableExample',
-                        table: {
-                            headerRows: 1,
-                            body: [
-                                [{ text: 'Header 1', style: 'tableHeader' }, { text: 'Header 2', style: 'tableHeader'}, { text: 'Header 3', style: 'tableHeader' }],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                            ]
-                        },
-                        layout: 'lightHorizontalLines'
-                    },
-                    { text: 'but you can provide a custom styler as well', margin: [0, 20, 0, 8] },
-                    {
-                        style: 'tableExample',
-                        table: {
-                            headerRows: 1,
-                            body: [
-                                [{ text: 'Header 1', style: 'tableHeader' }, { text: 'Header 2', style: 'tableHeader'}, { text: 'Header 3', style: 'tableHeader' }],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                                [ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-                            ]
-                        },
-                        layout: {
-                            hLineWidth: function(i, node) {
-                                return (i === 0 || i === node.table.body.length) ? 2 : 1;
-                            },
-                            vLineWidth: function(i, node) {
-                                return (i === 0 || i === node.table.widths.length) ? 2 : 1;
-                            },
-                            hLineColor: function(i, node) {
-                                return (i === 0 || i === node.table.body.length) ? 'black' : 'gray';
-                            },
-                            vLineColor: function(i, node) {
-                                return (i === 0 || i === node.table.widths.length) ? 'black' : 'gray';
-                            },
-                            // paddingLeft: function(i, node) { return 4; },
-                            // paddingRight: function(i, node) { return 4; },
-                            // paddingTop: function(i, node) { return 2; },
-                            // paddingBottom: function(i, node) { return 2; }
-                        }
-                    }
-                ],
-                styles: {
-                    header: {
-                        fontSize: 18,
-                        bold: true,
-                        margin: [0, 0, 0, 10]
-                    },
-                    subheader: {
-                        fontSize: 16,
-                        bold: true,
-                        margin: [0, 10, 0, 5]
-                    },
-                    tableExample: {
-                        margin: [0, 5, 0, 15]
-                    },
-                    tableHeader: {
-                        bold: true,
-                        fontSize: 13,
-                        color: 'black'
-                    }
+            var style = {
+                header: {
+                    fontSize: 18,
+                    bold: true,
+                    margin: [0, 0, 0, 10]
                 },
-                defaultStyle: {
-                    // alignment: 'justify'
+                subheader: {
+                    fontSize: 16,
+                    bold: true,
+                    margin: [0, 10, 0, 5]
+                },
+                tableExample: {
+                    margin: [0, 5, 0, 15]
+                },
+                tableHeader: {
+                    bold: true,
+                    fontSize: 13,
+                    color: 'green'
                 }
+            };
 
+            var docDefinition =  {
+                content: {
+                    style: 'tableExample',
+                    widths: [ '20%', '5%', '5%', "40%", '15%', '10%', "5%"],
+                    table: { headerRows: 1, body: [] }
+                },
+                styles: style,
+                pageOrientation: 'landscape'
+            };
+
+            function createTable(audits) {
+                addHeader();
+                for (var i = 0; i < audits.length; i++) {
+                   addRow(audits[i]);
+                }
             }
+
+            function addHeader() {
+                var headers = ['Entity Id', 'Action', 'Version', 'Value',
+                    'Modified Date', 'Modified By', 'View Details'];
+                var tableHeader = [];
+                for (var i = 0; i < headers.length; i++) {
+                    var object = { text: headers[i], style: 'tableHeader' };
+                    tableHeader.push(object);
+                }
+                docDefinition.content.table.body[0] = tableHeader;
+            }
+
+            function addRow(auditRecord) {
+                var properties = ['entityId', 'action', 'commitVersion', 'entityValue',
+                    'modifiedDate', 'modifiedBy'];
+                var row = [];
+                for (var i = 0; i < properties.length; i++) {
+                    if (properties[i] == 'entityValue')
+                        row.push(JSON.stringify(auditRecord[properties[i]], null, 2));
+                    else
+                        row.push(auditRecord[properties[i]]);
+
+                }
+                row.push('eye');
+                docDefinition.content.table.body.push(row);
+            }
+
+            createTable(vm.audits);
 
             // open the PDF in a new window
             pdfMake.createPdf(docDefinition).open();
