2dcc0861ff0d7982a9c244b9f09f1224d28e8bed
Steven <smep77@mail.umkc.edu>
2016-05-02 22:32:02 -0500

    Work on deployment problems

--- src/main/java/com/commercebank/www/config/CloudDatabaseConfiguration.java
+++ src/main/java/com/commercebank/www/config/CloudDatabaseConfiguration.java
@@ -2,16 +2,12 @@ package com.commercebank.www.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.*;
 import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
 @Profile(Constants.SPRING_PROFILE_CLOUD)
-@Import(value = MongoAutoConfiguration.class)
-@EnableMongoAuditing(auditorAwareRef = "springSecurityAuditorAware")
 public class CloudDatabaseConfiguration extends AbstractCloudConfig {
 
     private final Logger log = LoggerFactory.getLogger(CloudDatabaseConfiguration.class);
--- src/main/java/com/commercebank/www/repository/GovRecRepository.java
+++ src/main/java/com/commercebank/www/repository/GovRecRepository.java
@@ -1,18 +1,14 @@
 package com.commercebank.www.repository;
 
-import com.commercebank.www.domain.ACHCase;
 import com.commercebank.www.domain.GovRec;
 
 import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 /**
  * Spring Data MongoDB repository for the GovRec entity.
  */
-//@JaversSpringDataAuditable
+@JaversSpringDataAuditable
 public interface GovRecRepository extends MongoRepository<GovRec,String> {
 
    // List<ACHCase> findByCompletedOnBetween(LocalDateTime localDateTime, LocalDateTime localDateTime1);
